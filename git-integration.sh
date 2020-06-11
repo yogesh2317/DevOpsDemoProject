83b980d5d094ad29e838f6b33edc69d78622d76a
yogesh2317 <66724245+yogesh2317@users.noreply.github.com>
2020-06-11 16:31:36 +0530

    Maven & Python install

--- git-integration.sh
+++ git-integration.sh
@@ -1,3 +1,10 @@
 echo "my first ever github integration with jenkins"
 echo "test again"
 echo "I'm lovin it"
+yum install -mvn
+
+echo "=============================="
+yum install -y https://repo.ius.io/ius-release-el7.rpm
+yum update -y
+yum install -y python36u python36u-libs python36u-devel python36u-pip
+exit 0
